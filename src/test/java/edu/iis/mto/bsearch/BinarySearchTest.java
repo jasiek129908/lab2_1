e7db8be485ab638b3964f1d13925166a8820d7cf
Jan Oleksik <222529@edu.p.lodz.pl>
2021-03-15 19:04:28 +0100

    testy_v1 czy jest srodkowym elementem

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -16,15 +16,15 @@ class BinarySearchTest {
 
     @Test
     void valueIsInSequence() {
-        int array[] = {1, 10, 12, 14};
-        int searchValue = 10;
+        int array[] = {1};
+        int searchValue = 1;
         searchResult = BinarySearch.search(searchValue, array);
         assertTrue(searchResult.isFound());
     }
 
     @Test
     void valueIsNotInSequence() {
-        int array[] = {1, 10, 12, 14};
+        int array[] = {12};
         int searchValue = 19;
         searchResult = BinarySearch.search(searchValue, array);
         assertFalse(searchResult.isFound());
@@ -44,6 +44,14 @@ class BinarySearchTest {
         int searchValue = 14;
         searchResult = BinarySearch.search(searchValue, array);
         assertEquals(array.length, searchResult.getPosition());
+    }
+
+    @Test
+    void valueIsCentralElementInArray() {
+        int array[] = {1, 10, 12};
+        int searchValue = 10;
+        searchResult = BinarySearch.search(searchValue, array);
+        assertEquals((array.length / 2) + 1, searchResult.getPosition());
     }
 
 }
